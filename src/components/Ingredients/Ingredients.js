837f4fdcab9319617eb50ee911be8302068523ef
wimerc7 <william.mercado@capitalone.com>
2022-01-30 23:12:20 -0600

    First commit

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://react-hooks-update-677d7-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
+
+  const onRemoveItemHandler = (ingredientId) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+    );
+  };
+
+  const addIngredientsHandler = (ingredient) => {
+    fetch(
+      "https://react-hooks-update-677d7-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        return response.json;
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
+  };
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientsHandler} />
+
+      <section>
+        <Search />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={onRemoveItemHandler}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
